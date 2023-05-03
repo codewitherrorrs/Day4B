029bbce51a3ffc7992f5c34ae35625592d60c814
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-03 14:33:48 +0530

    Add : Print Unique Element

--- Day4B/Program.cs
+++ Day4B/Program.cs
@@ -4,8 +4,10 @@
     {
         public static void Main(string[] args)
         {
-            DuplicateArrayItem duplicateArrayItem = new DuplicateArrayItem();
-            duplicateArrayItem.DuplicateElement();
+            /*DuplicateArrayItem duplicateArrayItem = new DuplicateArrayItem();
+            duplicateArrayItem.DuplicateElement();*/
+            UniqueElement uniqueElement= new UniqueElement();
+            uniqueElement.PrintUniqueElement();
             Console.ReadKey();
         }
     }
---
+++ Day4B/UniqueElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B
+{
+    public class UniqueElement
+    {
+        public void PrintUniqueElement()
+        {
+            int[] arr = { 1, 2, 3, 3, 4, 5, 5 };
+            Console.WriteLine("The unique elements in the array are:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isDuplicate = true;    
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
